cd52c2b8f5ea85b16603873a56f495e8f2510f63
Kaushik Iyer <kaushik.i@endurance.com>
2020-04-02 12:34:37 +0530

    cdc-renderer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  let [showLogo, setShowLogo] = useState(true);
+  let [color, setColor] = useState("red");
+  useEffect(() => {
+    let colors = ["red", "blue", "green"];
+    let i = 0;
+    let interval = setInterval(() => {
+      setColor(colors[++i % 3]);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="App">
+      so ideally this should not come
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {showLogo && <img src={logo} className="App-logo" alt="logo" />}
+        <p
+          onClick={_ => setShowLogo(showLogo => !showLogo)}
+          style={{ backgroundColor: color }}
+        >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -19,6 +35,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <input type="text" />
     </div>
   );
 }
