b805d1d286a87da52855c8e16523693dbc0055d6
maheshhp <hpmahesh73@gmail.com>
2020-04-04 20:57:51 +0530

    chore: add jest support to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,26 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    "jest/globals": true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    "plugin:react/recommended",
+    "airbnb",
   ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
   plugins: [
-    'react',
+    "react",
+    "jest"
   ],
   rules: {
     "no-console": "warn",
